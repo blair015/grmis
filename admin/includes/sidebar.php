43b57b6dadeb755d07c4d7c3ffe12021bc722e56
SerTabz <rannie.jhon25@gmail.com>
2023-09-28 11:11:31 +0800

    Update sidebar.php

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -8,7 +8,9 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
+                            
                             <div class="sb-sidenav-menu-heading">Interface</div>
+
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 School Profile
@@ -22,36 +24,33 @@
                                 </nav>
                             </div>
                             
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts1" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Transaction
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+
+                            <div class="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="Approval.php">Schools for Approval</a>
+                                    
+                                </nav>
+                            </div>
+
                            
-                            <?php
-include("admin/config/dbcon.php");
 
-// Query to count rows in your table
-$sql = "SELECT COUNT(*) as row_count FROM approval"; // Change "your_table" to your actual table name
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Fetch the count
-    $row = $result->fetch_assoc();
-    $rowCount = $row["row_count"];
-    
-    // Close the database connection
-    $conn->close();
-} else {
-    echo "No records found";
-    // Close the database connection
-    $conn->close();
-}
-?>
-
+<!-- 
 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
     <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
     <a href="Approval.php">Pages</a>
+    
+    
+    
     <div class="notification-bell">
-        <i class="fa fa-bell"></i>
+    <i class="fa fa-bell"></i>
         <span class="notification-badge"><?php echo $rowCount; ?></span>
     </div>
-</a>
+    </a> -->
 
 
                                 <!-- <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
