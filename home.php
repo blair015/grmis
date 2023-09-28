b8c67b4d678d5678af1e40e9c680ff2f50cda979
SerTabz <rannie.jhon25@gmail.com>
2023-09-28 16:02:45 +0800

    design home

--- home.php
+++ home.php
@@ -9,14 +9,19 @@
         <!-- Favicon-->
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/mycss.css" rel="stylesheet" />
+
     </head>
     <body>
 
         <!-- Header - set the background image for the header in the line below-->
-        <header class="py-5 bg-image-full" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1600x900')">
+        <header class="py-5 bg-image-full" style="background-image: url('assets/images/background.png'); background-size: 100% 100%;">
+
             <div class="text-center my-5">
             <img class="img-fluid rounded-circle mb-4" src="assets/images/gov.png" alt="grmis logo" width="200" height="200" />
-                <h1 class="text-white fs-3 fw-bolder">Governance Resource Management and Information System</h1>
+                <h2 class="text-white fs-1 fw text-white-50">Welcome to</h2>
+                <h1 class="text-white fs-1 fw-bolder">Governance Resource Management and Information System</h1>
+                
                 <a class="button-77" role="button" href="index.php">Proceed</a>
                 <!-- <p class="text-white-50 mb-0">Landing Page Template</p> -->
             </div>
@@ -26,9 +31,13 @@
             <div class="container my-5">
                 <div class="row justify-content-center">
                     <div class="col-lg-6">
-                        <h2>Full Width Backgrounds</h2>
+                        <!-- <h2>Full Width Backgrounds</h2>
                         <p class="lead">A single, lightweight helper class allows you to add engaging, full width background images to sections of your page.</p>
-                        <p class="mb-0">The universe is almost 14 billion years old, and, wow! Life had no problem starting here on Earth! I think it would be inexcusably egocentric of us to suggest that we're alone in the universe.</p>
+                        <p class="mb-0">The universe is almost 14 billion years old, and, wow! Life had no problem starting here on Earth! I think it would be inexcusably egocentric of us to suggest that we're alone in the universe.</p> -->
+                        
+                        <img  src="assets/images/davsur1.png" alt="grmis logo" width="100%" height="100%" />
+                    
+                    
                     </div>
                 </div>
             </div>
@@ -36,116 +45,13 @@
    
     
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; ProjecDavSur 2023</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        <!-- <script src="js/scripts.js"></script> -->
     </body>
 </html>
 
 
-/* CSS */
-<style>
-.button-77 {
-  align-items: center;
-  appearance: none;
-  background-clip: padding-box;
-  background-color: initial;
-  background-image: none;
-  border-style: none;
-  box-sizing: border-box;
-  color: #fff;
-  cursor: pointer;
-  display: inline-block;
-  flex-direction: row;
-  flex-shrink: 0;
-  font-family: Eina01,sans-serif;
-  font-size: 16px;
-  font-weight: 800;
-  justify-content: center;
-  line-height: 24px;
-  margin: 0;
-  min-height: 64px;
-  outline: none;
-  overflow: visible;
-  padding: 19px 26px;
-  pointer-events: auto;
-  position: relative;
-  text-align: center;
-  text-decoration: none;
-  text-transform: none;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  vertical-align: middle;
-  width: auto;
-  word-break: keep-all;
-  z-index: 0;
-}
-
-@media (min-width: 768px) {
-  .button-77 {
-    padding: 19px 32px;
-  }
-}
-
-.button-77:before,
-.button-77:after {
-  border-radius: 80px;
-}
-
-.button-77:before {
-  background-color: rgba(249, 58, 19, .32);
-  content: "";
-  display: block;
-  height: 100%;
-  left: 0;
-  overflow: hidden;
-  position: absolute;
-  top: 0;
-  width: 100%;
-  z-index: -2;
-}
-
-.button-77:after {
-  background-color: initial;
-  background-image: linear-gradient(92.83deg, #ff7426 0, #f93a13 100%);
-  bottom: 4px;
-  content: "";
-  display: block;
-  left: 4px;
-  overflow: hidden;
-  position: absolute;
-  right: 4px;
-  top: 4px;
-  transition: all 100ms ease-out;
-  z-index: -1;
-}
-
-.button-77:hover:not(:disabled):after {
-  bottom: 0;
-  left: 0;
-  right: 0;
-  top: 0;
-  transition-timing-function: ease-in;
-}
-
-.button-77:active:not(:disabled) {
-  color: #ccc;
-}
-
-.button-77:active:not(:disabled):after {
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, .2), rgba(0, 0, 0, .2)), linear-gradient(92.83deg, #ff7426 0, #f93a13 100%);
-  bottom: 4px;
-  left: 4px;
-  right: 4px;
-  top: 4px;
-}
-
-.button-77:disabled {
-  cursor: default;
-  opacity: .24;
-}
-</style>
