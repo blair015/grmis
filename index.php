a0909c265a5345208d170aed66f48b039e40ef64
blair015 <tblairbrian@gmail.com>
2023-09-28 19:23:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -186,7 +186,7 @@ $totals = $totalMaleTeachers + $totalFemaleTeachers;
                 <div class="col-md-6">
                     <div class="card card-success">
                         <div class="card-header">
-                            <h3 class="card-title">Total Number of Enrollees</h3>
+                            <h3 class="card-title">Total Number of Enrolled Students</h3>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                     <i class="fas fa-minus"></i>
