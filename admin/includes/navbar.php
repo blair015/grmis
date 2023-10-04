6ccb440cc3f5793779f93ab24a4473c08689a17f
blair015 <tblairbrian@gmail.com>
2023-10-04 19:14:39 +0800

    Update navbar.php

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -95,10 +95,10 @@ if ($stmt) {
 include ("admin/includes/phpscript.php");
 ?>
       <div class="notification-bell">
-    <a class="fa fa-bell" href="approval.php"></a>
+    <a class="fa fa-bell" href="approval.php">
         <span class="notification-badge"><?php echo $rowCount; ?></span>
-    </div>
     </a>
+</div>
     </form>
 
     <!-- Navbar-->
