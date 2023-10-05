eb39e1fc8edba403127587f696bce79fd2d04cf0
blair015 <tblairbrian@gmail.com>
2023-10-05 19:16:55 +0800

    Update view.php

--- view.php
+++ view.php
@@ -206,12 +206,14 @@ include ("admin/includes/sidebar.php");
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel">Add School</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
+                        <div class="modal-header" style="background-color: blue; color: white;">
+                    <h4 class="modal-title" id="myModalLabel">
+                        <i class="fas fa-plus-circle" style="margin-right: 10px;"></i> Add School
+                    </h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="color: white;">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
             <div class="modal-body">
             
         <?php
