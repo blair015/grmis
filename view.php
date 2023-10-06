5393865b7deea81bd9ac129c6bf19c1699236dc3
blair015 <tblairbrian@gmail.com>
2023-10-06 08:17:25 +0800

    Update view.php

--- view.php
+++ view.php
@@ -183,8 +183,9 @@ include ("admin/includes/sidebar.php");
                                             echo "<td>" . $row['District'] . "</td>";
                                             echo "<td>" . $row['category'] . "</td>";
                                             echo "<td>
-                                            <a href='schoolprofile.php?school_id=" . $row['school_id']"'><i class='fas fa-eye fa-2x'></i></a>
-                                                 </td>";
+                                                <a href='schoolprofile.php?school_id=" . $row['school_id'] . "'><i class='fas fa-eye fa-2x'></i></a>
+                                            </td>";
+
                                         
 
                                             echo "</tr>";
