6ef221346f2958219d96e6818bee60d0aaab0a33
blair015 <tblairbrian@gmail.com>
2023-10-20 22:46:34 +0800

    Update schoolprofile.php

--- schoolprofile.php
+++ schoolprofile.php
@@ -449,7 +449,7 @@ if (isset($_GET['school_id'])) {
                         <div class="card">
                             <div class="card-body text-center">
                                 <img src="<?php echo $imageUrl; ?>" alt="Teacher's Picture"
-                        class="rounded-circle img-fluid" style="width: 150px;">
+                                class="rounded-circle img-fluid" style="width: 150px; height: 150px;">
                         <h6 class="my-3"><?php echo $row['firstname'] . ' ' . $row['middlename'] . ' ' . $row['lastname']; ?></h6>
                         <p class="text-muted mb-1"><?php echo $row['position_rank']; ?></p>
                 </div>
