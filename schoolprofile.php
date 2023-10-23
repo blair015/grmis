9f8b002a4f1d38013fcba882e54045c36ceef356
blair015 <tblairbrian@gmail.com>
2023-10-23 11:29:25 +0800

    Update schoolprofile.php

--- schoolprofile.php
+++ schoolprofile.php
@@ -528,14 +528,15 @@ if (isset($_GET['school_id'])) {
     </div>
     
             </div>
-            <div class="tab-pane" id="pf">
+           
+
+
+                </div>
+                <div class="tab-pane" id="pf">
     <!-- Add your content for the Physical Facilities tab here -->
     <h3>Physical Facilities</h3>
     <p>This is the content for the Physical Facilities tab.</p>
 </div>
-
-
-                </div>
               <!-- /.tab-pane -->
               </div>
               <!-- /.tab-content -->
