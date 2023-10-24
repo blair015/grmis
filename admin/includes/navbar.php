ea49a700d2aa484402626b7af08e883ae0219d70
blair015 <tblairbrian@gmail.com>
2023-10-24 08:41:02 +0800

    Update navbar.php

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -122,7 +122,7 @@ include ("admin/includes/phpscript.php");
 
     <!-- Navbar-->
 
-    <?php echo $fname ." ". $lname ?>
+    <!-- <?php echo $fname ." ". $lname ?> -->
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
