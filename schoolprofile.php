e3926b53079c1972fee55ed82ed3293db2d59aa9
blair015 <tblairbrian@gmail.com>
2023-10-27 15:51:24 +0800

    Update schoolprofile.php

--- schoolprofile.php
+++ schoolprofile.php
@@ -305,6 +305,12 @@ if (isset($_GET['school_id'])) {
         width: 100%;
         height: 100%;
     }
+    /* Define a custom class for the active tab */
+.nav-pills .nav-link.active {
+    background-color: #FFA500; /* Orange background color */
+    color: #fff; /* Text color for the active tab */
+}
+
 </style>
 
 
