8417166e8b333d2b9097d7d51dc4c8475f54b48f
blair015 <tblairbrian@gmail.com>
2023-11-06 15:23:40 +0800

    Update schoolprofile.php

--- schoolprofile.php
+++ schoolprofile.php
@@ -636,66 +636,70 @@ $selectedSchoolId2 = $_GET['school_id'];
 
 
 ?>
-                 <style>
-                    .dropdown-menu li {
-                        position: relative;
-                        }
-                        .dropdown-menu .dropdown-submenu {
-                        display: none;
-                        position: absolute;
-                        left: 100%;
-                        top: -7px;
-                        }
-                        .dropdown-menu .dropdown-submenu-left {
-                        right: 100%;
-                        left: auto;
-                        }
-                        .dropdown-menu > li:hover > .dropdown-submenu {
-                        display: block;
-                        }
-                    </style>
-                    <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
-                        data-mdb-toggle="dropdown" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
+                <style>
+    .dropdown-menu li {
+        position: relative;
+    }
+
+    .dropdown-menu .dropdown-submenu {
+        display: none;
+        position: absolute;
+        left: 100%;
+        top: -7px;
+    }
+
+    .dropdown-menu .dropdown-submenu-left {
+        right: 100%;
+        left: auto;
+    }
+
+    .dropdown-menu > li:hover > .dropdown-submenu {
+        display: block;
+    }
+</style>
+
+<div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+        Dropdown button
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li>
+            <a class="dropdown-item" href="#">Another action</a>
+        </li>
+        <li>
+            <a class="dropdown-item" href="#">
+                Submenu &raquo;
+            </a>
+            <ul class="dropdown-menu dropdown-submenu">
+                <li>
+                    <a class="dropdown-item" href="#">Submenu item 1</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#">Submenu item 2</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#">Submenu item 3 &raquo; </a>
+                    <ul class="dropdown-menu dropdown-submenu">
                         <li>
-                        <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Multi level 1</a>
                         </li>
                         <li>
-                        <a class="dropdown-item" href="#">
-                            Submenu &raquo;
-                        </a>
-                        <ul class="dropdown-menu dropdown-submenu">
-                            <li>
-                            <a class="dropdown-item" href="#">Submenu item 1</a>
-                            </li>
-                            <li>
-                            <a class="dropdown-item" href="#">Submenu item 2</a>
-                            </li>
-                            <li>
-                            <a class="dropdown-item" href="#">Submenu item 3 &raquo; </a>
-                            <ul class="dropdown-menu dropdown-submenu">
-                                <li>
-                                <a class="dropdown-item" href="#">Multi level 1</a>
-                                </li>
-                                <li>
-                                <a class="dropdown-item" href="#">Multi level 2</a>
-                                </li>
-                            </ul>
-                            </li>
-                            <li>
-                            <a class="dropdown-item" href="#">Submenu item 4</a>
-                            </li>
-                            <li>
-                            <a class="dropdown-item" href="#">Submenu item 5</a>
-                            </li>
-                        </ul>
+                            <a class="dropdown-item" href="#">Multi level 2</a>
                         </li>
                     </ul>
-                    </div>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#">Submenu item 4</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="#">Submenu item 5</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
                
 
     <button type="button" class="btn btn-success" id="Buttonedit" data-toggle="modal" data-target="#editModal">
