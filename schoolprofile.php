5a65b5c3bb5b94b09f8cbd63982a3513a165292f
blair015 <tblairbrian@gmail.com>
2023-11-06 15:21:25 +0800

    Update schoolprofile.php

--- schoolprofile.php
+++ schoolprofile.php
@@ -34,6 +34,10 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username']) && isset($_SESSI
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 
 <?php
