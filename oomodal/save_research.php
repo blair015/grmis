0c44bb0274b1ac217abe5459b6ed824c6e9d28af
blair015 <tblairbrian@gmail.com>
2023-11-07 18:43:40 +0800

    Update save_research.php

--- oomodal/save_research.php
+++ oomodal/save_research.php
@@ -1,6 +1,39 @@
 <?php
 session_start();
 
+// if ($_SERVER["REQUEST_METHOD"] === "POST") {
+//     include('../admin/config/dbcon.php'); // Include your database connection script
+
+//     $schoolId = $_POST['schoolId'];
+//     $researchCompleted = $_POST['researchCompleted'];
+//     $quarter = $_POST['quarter'];
+//     $schoolYear = $_POST['schoolYear']; // Retrieve the selected school year
+//     $user_school_id = isset($_SESSION['school_id']) ? $_SESSION['school_id'] : '';
+
+//     // You should perform some validation and sanitization of input data here to prevent SQL injection
+
+//     // Assuming your table is named "oo_research", you can use a prepared statement to insert the data into the table.
+//     $stmt = $conn->prepare("INSERT INTO oo_research (school_id, research_completed, quarter, school_year) VALUES (?, ?, ?, ?)");
+//     $stmt->bind_param("isss", $schoolId, $researchCompleted, $quarter, $schoolYear);
+
+//     if ($stmt->execute()) {
+//         // Data has been successfully inserted
+//         echo "Data saved successfully.";
+//         echo '<script>alert("Data saved successfully.");</script>';
+//         echo '<script>setTimeout(function() { window.location = "../schoolprofile.php?school_id=' . $schoolId . '&user_school_id=' . $user_school_id . '"; }, 1000);</script>';
+//     } else {
+//         // Error occurred while inserting data
+//         echo "Error: " . $stmt->error;
+//         echo '<script>setTimeout(function() { window.location = "../schoolprofile.php?school_id=' . $schoolId . '&user_school_id=' . $user_school_id . '"; }, 1000);</script>';
+//     }
+
+//     $stmt->close();
+//     $conn->close();
+// } else {
+//     // If the request is not POST, you can handle it as needed.
+//     echo "Invalid request method.";
+// }
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     include('../admin/config/dbcon.php'); // Include your database connection script
 
@@ -12,7 +45,43 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // You should perform some validation and sanitization of input data here to prevent SQL injection
 
-    // Assuming your table is named "oo_research", you can use a prepared statement to insert the data into the table.
+    // Check if data already exists for the given school year and quarter
+    $checkQuery = "SELECT * FROM oo_research WHERE school_id = ? AND quarter = ? AND school_year = ?";
+    $checkStmt = $conn->prepare($checkQuery);
+    $checkStmt->bind_param("iss", $schoolId, $quarter, $schoolYear);
+    $checkStmt->execute();
+    $result = $checkStmt->get_result();
+    
+    if ($result->num_rows > 0) {
+        // Data already exists, prompt the user for confirmation
+        echo "Data for the selected school year and quarter already exists. Do you want to overwrite it?";
+        echo '<form method="post">';
+        echo '<input type="submit" name="confirm_overwrite" value="Yes">';
+        echo '<input type="submit" name="cancel_overwrite" value="No">';
+        echo '</form>';
+    } else {
+        // Data does not exist, proceed with insertion
+        insertData($conn, $schoolId, $researchCompleted, $quarter, $schoolYear, $user_school_id);
+    }
+
+    $checkStmt->close();
+    $conn->close();
+} elseif (isset($_POST['confirm_overwrite']) && $_POST['confirm_overwrite'] === "Yes") {
+    // User confirmed overwrite, proceed with insertion
+    include('../admin/config/dbcon.php');
+    $schoolId = $_POST['schoolId'];
+    $researchCompleted = $_POST['researchCompleted'];
+    $quarter = $_POST['quarter'];
+    $schoolYear = $_POST['schoolYear'];
+    $user_school_id = isset($_SESSION['school_id']) ? $_SESSION['school_id'] : '';
+
+    insertData($conn, $schoolId, $researchCompleted, $quarter, $schoolYear, $user_school_id);
+} else {
+    // If the request is not POST, you can handle it as needed.
+    echo "Invalid request method.";
+}
+
+function insertData($conn, $schoolId, $researchCompleted, $quarter, $schoolYear, $user_school_id) {
     $stmt = $conn->prepare("INSERT INTO oo_research (school_id, research_completed, quarter, school_year) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("isss", $schoolId, $researchCompleted, $quarter, $schoolYear);
 
@@ -28,9 +97,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
     $stmt->close();
-    $conn->close();
-} else {
-    // If the request is not POST, you can handle it as needed.
-    echo "Invalid request method.";
 }
+
+
 ?>
