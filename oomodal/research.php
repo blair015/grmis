5ccb8a71694dad498f481f31666010a5c8807739
blair015 <tblairbrian@gmail.com>
2023-11-07 19:39:48 +0800

    Update research.php

--- oomodal/research.php
+++ oomodal/research.php
@@ -2,16 +2,74 @@
 <div class="modal fade" id="researchCompletedModal" tabindex="-1" role="dialog" aria-labelledby="researchCompletedModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <!-- Modal content remains the same as in your original code -->
+            <div class="modal-header">
+                <h5 class="modal-title" id="researchCompletedModalLabel">Research Completed Modal</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="researchForm" method="post" action="oomodal/save_research.php">
+                    <?php
+                    if (isset($_GET['school_id'])) {
+                        $_SESSION['school_id'] = $_GET['school_id'];
+                    } else {
+                        echo "School identifier is missing.";
+                        exit;
+                    }
+                    include ('admin/config/dbcon.php');
+                    $school_id = $_SESSION['school_id'];
+                    echo $school_id;
+                    ?>
+                    <input type="hidden" id="schoolId" name="schoolId" value="<?php echo $school_id; ?>">
+                    <div class="form-group">
+                        <label for="researchCompleted">Research Completed</label>
+                        <input type="text" id="researchCompleted" name="researchCompleted" class="form-control" placeholder="Enter Research Completed">
+                    </div>
+                    <div class="form-group">
+                        <label>Quarters</label>
+                        <div class="row">
+                            <div class="col">
+                                <label for="quarter1">Q1</label>
+                                <input type="radio" id="quarter1" name="quarter" value="1">
+                            </div>
+                            <div class="col">
+                                <label for="quarter2">Q2</label>
+                                <input type="radio" id="quarter2" name="quarter" value="2">
+                            </div>
+                            <div class="col">
+                                <label for ="quarter3">Q3</label>
+                                <input type="radio" id="quarter3" name="quarter" value="3">
+                            </div>
+                            <div class="col">
+                                <label for="quarter4">Q4</label>
+                                <input type="radio" id="quarter4" name="quarter" value="4">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="schoolYear">School Year</label>
+                        <select id="schoolYear" name="schoolYear" class="form-control">
+                            <?php
+                            // Generate options for school years from 2023-2024 to 2030
+                            for ($year = 2023; $year <= 2030; $year++) {
+                                $nextYear = $year + 1;
+                                $schoolYear = $year . '-' . $nextYear;
+                                echo '<option value="' . $schoolYear . '">' . $schoolYear . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" id="saveResearchBtn" class="btn btn-primary">Save</button>
+                        </div>
 
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" id="saveResearchBtn" class="btn btn-primary">Save</button>
+                </form>
             </div>
         </div>
     </div>
 </div>
-
 <!-- Include jQuery and Bootstrap scripts -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
