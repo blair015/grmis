c8197541e294bfbf27b9c9c4a238caa6b12f4f97
blair015 <tblairbrian@gmail.com>
2023-11-07 21:50:39 +0800

    Update save_research.php

--- oomodal/save_research.php
+++ oomodal/save_research.php
@@ -7,45 +7,20 @@ include('../admin/includes/script.php');
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Include your database connection script
     $schoolId = $_POST['schoolId'];
-    $researchCompleted = $_POST['researchCompleted'];
-    $quarter = $_POST['quarter'];
-    $schoolYear = $_POST['schoolYear']; // Retrieve the selected school year
     $user_school_id = isset($_SESSION['school_id']) ? $_SESSION['school_id'] : '';
 
-    // You should perform some validation and sanitization of input data here to prevent SQL injection
+    if (isset($_GET['confirm_overwrite']) && $_GET['confirm_overwrite'] === "1") {
+        // User confirmed overwrite, proceed with insertion
+        $researchCompleted = $_POST['researchCompleted'];
+        $quarter = $_POST['quarter'];
+        $schoolYear = $_POST['schoolYear'];
 
-    // Check if data already exists for the given school year and quarter
-    $checkQuery = "SELECT * FROM oo_research WHERE school_id = ? AND quarter = ? AND school_year = ?";
-    $checkStmt = $conn->prepare($checkQuery);
-    $checkStmt->bind_param("iss", $schoolId, $quarter, $schoolYear);
-    $checkStmt->execute();
-    $result = $checkStmt->get_result();
-
-    if ($result->num_rows > 0) {
-        // Data already exists, prompt the user for confirmation using JavaScript
-        echo '<script>
-            var confirmOverwrite = confirm("Data for the selected school year and quarter already exists. Do you want to overwrite it?");
-            if (confirmOverwrite) {
-                window.location = "?school_id=' . $schoolId . '&confirm_overwrite=1";
-            } else {
-                window.location = "?school_id=' . $schoolId . '";
-            }
-        </script>';
-    } else {
-        // Data does not exist, proceed with insertion
         insertData($conn, $schoolId, $researchCompleted, $quarter, $schoolYear, $user_school_id);
+    } else {
+        // If the request is not POST or if confirm_overwrite is not set, you can handle it as needed.
+        echo "Invalid request method.";
     }
-} elseif (isset($_GET['confirm_overwrite']) && $_GET['confirm_overwrite'] === "1") {
-    // User confirmed overwrite, proceed with insertion
-    $schoolId = $_GET['school_id'];
-    $researchCompleted = $_POST['researchCompleted'];
-    $quarter = $_POST['quarter'];
-    $schoolYear = $_POST['schoolYear'];
-    $user_school_id = isset($_SESSION['school_id']) ? $_SESSION['school_id'] : '';
-
-    insertData($conn, $schoolId, $researchCompleted, $quarter, $schoolYear, $user_school_id);
 } else {
-    // If the request is not POST, you can handle it as needed.
     echo "Invalid request method.";
 }
 
