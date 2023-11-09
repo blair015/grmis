4a15eb9d941868f6cf4543ddd36c62f760333436
blair015 <tblairbrian@gmail.com>
2023-11-09 19:21:33 +0800

    Update oo_report.php

--- oomodal/oo_report.php
+++ oomodal/oo_report.php
@@ -1,11 +1,13 @@
 <?php
 require('../fpdf/fpdf.php');
 include '../admin/config/dbcon.php';
+
 class PDF extends FPDF
 {
     function Header()
     {
         $this->SetFont('Arial', 'B', 12);
+        // Merge cells from the first column to the last column in the first row
         $this->Cell(0, 10, 'Table with 5 columns and 10 rows', 0, 1, 'C');
     }
 
@@ -25,11 +27,8 @@ $columnWidths = array(40, 40, 40, 40, 40);
 
 // Header
 $pdf->SetFont('Arial', 'B', 10);
-$pdf->Cell($columnWidths[0], 10, 'Column 1', 1);
-$pdf->Cell($columnWidths[1], 10, 'Column 2', 1);
-$pdf->Cell($columnWidths[2], 10, 'Column 3', 1);
-$pdf->Cell($columnWidths[3], 10, 'Column 4', 1);
-$pdf->Cell($columnWidths[4], 10, 'Column 5', 1);
+// Skip individual cells for the first row
+$pdf->Cell($columnWidths[0] + $columnWidths[1] + $columnWidths[2] + $columnWidths[3] + $columnWidths[4], 10, '', 0, 1);
 $pdf->Ln();
 
 // Data
