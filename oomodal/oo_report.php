80df5b943d83c8b1ec27952a0f25b2e4515f006b
blair015 <tblairbrian@gmail.com>
2023-11-09 19:27:12 +0800

    Update oo_report.php

--- oomodal/oo_report.php
+++ oomodal/oo_report.php
@@ -7,7 +7,8 @@ class PDF extends FPDF
     function Header()
     {
         $this->SetFont('Arial', 'B', 12);
-        // Merge cells from the first column to the last column in the first row
+        // Add a row at the top with "PAPS" centered across all columns
+        $this->Cell(array_sum($this->columnWidths), 10, 'PAPS', 0, 1, 'C');
         $this->Cell(0, 10, 'Table with 5 columns and 10 rows', 0, 1, 'C');
     }
 
@@ -24,11 +25,15 @@ $pdf->AddPage();
 
 // Column widths
 $columnWidths = array(40, 40, 40, 40, 40);
+$pdf->columnWidths = $columnWidths; // Assign column widths to the class property
 
 // Header
 $pdf->SetFont('Arial', 'B', 10);
-// Skip individual cells for the first row
-$pdf->Cell($columnWidths[0] + $columnWidths[1] + $columnWidths[2] + $columnWidths[3] + $columnWidths[4], 10, '', 0, 1);
+$pdf->Cell($columnWidths[0], 10, 'Column 1', 1);
+$pdf->Cell($columnWidths[1], 10, 'Column 2', 1);
+$pdf->Cell($columnWidths[2], 10, 'Column 3', 1);
+$pdf->Cell($columnWidths[3], 10, 'Column 4', 1);
+$pdf->Cell($columnWidths[4], 10, 'Column 5', 1);
 $pdf->Ln();
 
 // Data
