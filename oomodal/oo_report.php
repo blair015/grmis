f0c0612a12dd44fec43b1be99d5d104538d85acb
blair015 <tblairbrian@gmail.com>
2023-11-09 19:48:00 +0800

    Update oo_report.php

--- oomodal/oo_report.php
+++ oomodal/oo_report.php
@@ -4,23 +4,21 @@ include '../admin/config/dbcon.php';
 
 class PDF extends FPDF
 {
-    public $columnWidths; // Declare the property to store column widths
+    public $columnWidths = array(); // Initialize the property with an empty array
 
     function Header()
     {
         $this->SetFont('Arial', 'B', 12);
-        
-        // Check if columnWidths is set before using it
-        if (isset($this->columnWidths)) {
+
+        // Check if columnWidths is set and not empty before using it
+        if (!empty($this->columnWidths)) {
             // Add a row at the top with "PAPS" centered across all columns
             $this->Cell(array_sum($this->columnWidths), 10, 'PAPS', 1, 1, 'C'); // Use 1 for the border
         }
 
-        $this->Cell($this->columnWidths[0], 10, 'Column 1', 1);
-        $this->Cell($this->columnWidths[1], 10, 'Column 2', 1);
-        $this->Cell($this->columnWidths[2], 10, 'Column 3', 1);
-        $this->Cell($this->columnWidths[3], 10, 'Column 4', 1);
-        $this->Cell($this->columnWidths[4], 10, 'Column 5', 1);
+        foreach ($this->columnWidths as $width) {
+            $this->Cell($width, 10, '', 1);
+        }
         $this->Ln();
     }
 
@@ -42,11 +40,9 @@ $pdf->columnWidths = $columnWidths; // Assign column widths to the class propert
 // Data
 $pdf->SetFont('Arial', '', 10);
 for ($i = 1; $i <= 10; $i++) {
-    $pdf->Cell($columnWidths[0], 10, 'Row ' . $i . ', Col 1', 1);
-    $pdf->Cell($columnWidths[1], 10, 'Row ' . $i . ', Col 2', 1);
-    $pdf->Cell($columnWidths[2], 10, 'Row ' . $i . ', Col 3', 1);
-    $pdf->Cell($columnWidths[3], 10, 'Row ' . $i . ', Col 4', 1);
-    $pdf->Cell($columnWidths[4], 10, 'Row ' . $i . ', Col 5', 1);
+    foreach ($columnWidths as $width) {
+        $pdf->Cell($width, 10, 'Row ' . $i . ', Col', 1);
+    }
     $pdf->Ln();
 }
 
