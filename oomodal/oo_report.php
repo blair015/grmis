9168bee0a853be0a449adb14ab1dc97fa0e90491
blair015 <tblairbrian@gmail.com>
2023-11-13 22:19:26 +0800

    Update oo_report.php

--- oomodal/oo_report.php
+++ oomodal/oo_report.php
@@ -15,27 +15,26 @@ $school_id = $_SESSION['school_id'];
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the selected quarter and school year from the form
-    //$quarter = $_POST["quarter"];
+    // Get the selected school year from the form
     $schoolYear = $_POST["schoolYear"];
     $schoolId = $school_id;
 
     // Your SQL query should include a condition based on the user's selection
-$sql = "SELECT rs.research_completed, sp.retention_rate, sp.completion_rate, sp.nat_proportion, sp.feeding_program, sp.esc, sp.voucher, sp.joint_delivery,
-rt.ratio_teacher, rt.ratio_classroom, rt.ict_package1, lm.new_constructed, lm.on_going, lm.lm_procured, lm.scimath_package, lm.ict_package2, lm.tvl_package,
-lm.new_position, ic.sped, ic.sped_data, ic.iped, ic.iped_data, ic.alive, ic.alive_data, ic.als, ic.als_data, hm.lac, hm.teacher_trained, hm.related_trained
-FROM oo_research AS rs
-INNER JOIN oo_support AS sp ON rs.school_id = sp.school_id
-INNER JOIN oo_ratio AS rt ON sp.school_id = rt.school_id
-INNER JOIN oo_lm AS lm ON rt.school_id = lm.school_id
-INNER JOIN oo_inclusive AS ic ON lm.school_id = ic.school_id
-INNER JOIN oo_hrm AS hm ON ic.school_id = hm.school_id
-WHERE rs.school_id = ? AND rs.school_year = ?"; // Include quarter in the condition
-
+    $sql = "SELECT rs.research_completed, sp.retention_rate, sp.completion_rate, sp.nat_proportion, sp.feeding_program, sp.esc, sp.voucher, sp.joint_delivery,
+                rt.ratio_teacher, rt.ratio_classroom, rt.ict_package1, lm.new_constructed, lm.on_going, lm.lm_procured, lm.scimath_package, lm.ict_package2, lm.tvl_package,
+                lm.new_position, ic.sped, ic.sped_data, ic.iped, ic.iped_data, ic.alive, ic.alive_data, ic.als, ic.als_data, hm.lac, hm.teacher_trained, hm.related_trained,
+                rs.quarter
+        FROM oo_research AS rs
+        INNER JOIN oo_support AS sp ON rs.school_id = sp.school_id
+        INNER JOIN oo_ratio AS rt ON sp.school_id = rt.school_id
+        INNER JOIN oo_lm AS lm ON rt.school_id = lm.school_id
+        INNER JOIN oo_inclusive AS ic ON lm.school_id = ic.school_id
+        INNER JOIN oo_hrm AS hm ON ic.school_id = hm.school_id
+        WHERE rs.school_id = ? AND rs.school_year = ?";
 
     // Use a prepared statement to prevent SQL injection
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("is", $schoolId, $schoolYear); // Add $quarter to the binding parameters
+    $stmt->bind_param("is", $schoolId, $schoolYear);
 
     // Execute the query
     if (!$stmt->execute()) {
@@ -93,102 +92,52 @@ WHERE rs.school_id = ? AND rs.school_year = ?"; // Include quarter in the condit
 <form method="post" action="">
     <label for="schoolYear">Select School Year:</label>
     <input type="text" name="schoolYear" id="schoolYear" placeholder="Enter School Year">
-    
-    <!-- Add a select input for the quarter -->
-    <label for="quarter">Select Quarter:</label>
-    <select name="quarter" id="quarter">
-        <option value="1">1st Quarter</option>
-        <option value="2">2nd Quarter</option>
-        <option value="3">3rd Quarter</option>
-        <option value="4">4th Quarter</option>
-    </select>
-
     <button type="submit">Submit</button>
 </form>
 
-
-    <?php
-    // Check if there are rows in the result set
-    if ($result !== null) {
-        if ($result->num_rows > 0) {
-            // Fetch the result
-            $row = $result->fetch_assoc();
-
-           
-            ?>
-            <table>
-                <tr>
-                    <th colspan="5" style="text-align: center; height: 100px;">PAPS</th>
-                </tr>
-                <tr>
-                    <th colspan="5">EDUCATION POLICY DEVELOPMENT PROGRAM-(PPRD)</th>
-                </tr>
-                <tr>
-                    <td style="font-family: Arial, sans-serif; font-style: italic; ">Output Indicators</td>
-                    <?php
-                    // Dynamically create table headers based on the number of quarters
-                    for ($i = 1; $i <= 4; $i++) {
-                        echo "<td style='text-align: center;'>{$i}st Quarter</td>";
+<?php
+// Check if there are rows in the result set
+if ($result !== null) {
+    if ($result->num_rows > 0) {
+        // Fetch the result
+        $row = $result->fetch_assoc();
+        ?>
+        <table>
+            <tr>
+                <th colspan="5" style="text-align: center; height: 100px;">PAPS</th>
+            </tr>
+            <tr>
+                <th colspan="5">EDUCATION POLICY DEVELOPMENT PROGRAM-(PPRD)</th>
+            </tr>
+            <tr>
+                <td style="font-family: Arial, sans-serif; font-style: italic; ">Output Indicators</td>
+                <?php
+                // Dynamically create table headers based on the number of quarters
+                for ($i = 1; $i <= 4; $i++) {
+                    echo "<td style='text-align: center;'>{$i}st Quarter</td>";
+                }
+                ?>
+            </tr>
+            <tr>
+                <td>1. Number of education researches completed</td>
+                <?php
+                // Dynamically display data based on the quarter value
+                for ($i = 1; $i <= 4; $i++) {
+                    echo "<td>";
+                    if ($row['quarter'] == $i) {
+                        echo $row['research_completed'];
                     }
-                    ?>
-                </tr>
-                <tr>
-                    <td>1. Number of education researches completed</td>
-                    <?php
-                    // Dynamically display data based on the number of quarters
-                    for ($i = 1; $i <= 4; $i++) {
-                        echo "<td>{$row['research_completed']}</td>"; // Replace 'research_completed' with the actual column name
-                    }
-                    ?>
-                </tr>
-                <tr>
-                    <th colspan="5">BASIC EDUCATION INPUTS PROGRAM-(PPRD)</th>
-                </tr>
-                <tr>
-                    <td style="font-family: Arial, sans-serif; font-style: italic; ">Outcome Indicatorss</td>
-                    <?php
-                    // Dynamically create table headers based on the number of quarters
-                    for ($i = 1; $i <= 4; $i++) {
-                        echo "<td style='text-align: center;'>{$i}st Quarter</td>";
-                    }
-                    ?>
-                </tr>
-                <tr>
-                    <td>Percentage of schools meeting the standard ratio for teachers</td>
-                    <?php
-                    // Dynamically display data based on the number of quarters
-                    for ($i = 1; $i <= 4; $i++) {
-                        echo "<td>{$row['ratio_teacher']}</td>"; // Replace 'research_completed' with the actual column name
-                                            }
-                    ?>
-                </tr>
-                <tr>
-                    <td>Percentage of schools meeting the standard ratio for teachers</td>
-                    <?php
-                    // Dynamically display data based on the number of quarters
-                    for ($i = 1; $i <= 4; $i++) {
-                        echo "<td>{$row['ratio_classroom']}</td>";
-                    }
-                    ?>
-                </tr>
-                <tr>
-                    <td>Percentage of schools meeting the standard ratio for teachers</td>
-                    <?php
-                    // Dynamically display data based on the number of quarters
-                    for ($i = 1; $i <= 4; $i++) {
-                        
-                        echo "<td>{$row['ict_package1']}</td>";
-                    }
-                    ?>
-                </tr>
-                
-                <!-- Add more rows based on your data -->
-            </table>
+                    echo "</td>";
+                }
+                ?>
+            </tr>
+            <!-- Add more rows based on your data -->
+        </table>
         <?php
-        } else {
-            echo "<p>No results found.</p>";
-        }
+    } else {
+        echo "<p>No results found.</p>";
     }
-    ?>
+}
+?>
 </body>
 </html>
