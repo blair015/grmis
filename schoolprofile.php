c2b536e0de1002019429fc8e565b77d512143803
blair015 <tblairbrian@gmail.com>
2023-11-15 16:02:28 +0800

    Update schoolprofile.php

--- schoolprofile.php
+++ schoolprofile.php
@@ -712,7 +712,7 @@ include ('oomodal/hrm.php');
             </a>
             <ul class="dropdown-menu dropdown-submenu">
                 <li>
-                    <a class="dropdown-item" href="#">Outcome Indicators</a>
+                    <a class="dropdown-item" href="retentionEducationModal">Retention Rate</a>
                 </li>
                 <li>
                     <a class="dropdown-item" href="#">Output Indicators</a>
