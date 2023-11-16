5962ce6e87317c58be7e0452b50f142ac5d1d153
blair015 <tblairbrian@gmail.com>
2023-11-16 11:29:29 +0800

    Update retention.php

--- oomodal/retention.php
+++ oomodal/retention.php
@@ -43,7 +43,7 @@
                                 </div>
                             </div>
                         </div>
-
+                        <!-- //Elementary Input Field -->
                         <div class="form-group" id="grade1Input" style="display: none;">
                             <label for="grade1Text">Grade 1:</label>
                             <input type="text" id="grade1Text" name="grade1Text">
@@ -57,7 +57,7 @@
                             <input type="text" id="grade3Text" name="grade3Text">
                         </div>
                         <div class="form-group" id="grade4Input" style="display: none;">
-                            <label for="grade4Text">Grade 1:</label>
+                            <label for="grade4Text">Grade 4:</label>
                             <input type="text" id="grade4Text" name="grade4Text">
                         </div>
                         <div class="form-group" id="grade5Input" style="display: none;">
@@ -65,9 +65,60 @@
                             <input type="text" id="grade5Text" name="grade5Text">
                         </div>
                         <div class="form-group" id="grade6Input" style="display: none;">
-                            <label for="grade6Text">Grade 1:</label>
+                            <label for="grade6Text">Grade 6:</label>
                             <input type="text" id="grade6Text" name="grade6Text">
                         </div>
+                          <!-- //Secondary Input Field -->
+                          <div class="form-group" id="grade7Input" style="display: none;">
+                            <label for="grade7Text">Grade 7:</label>
+                            <input type="text" id="grade7Text" name="grade7Text">
+                        </div>
+                        <div class="form-group" id="grade8Input" style="display: none;">
+                            <label for="grade8Text">Grade 8:</label>
+                            <input type="text" id="grade8Text" name="grade8Text">
+                        </div>
+                        <div class="form-group" id="grade9Input" style="display: none;">
+                            <label for="grade9Text">Grade 9:</label>
+                            <input type="text" id="grade9Text" name="grade9Text">
+                        </div>
+                        <div class="form-group" id="grade10Input" style="display: none;">
+                            <label for="grade10Text">Grade 10:</label>
+                            <input type="text" id="grade10Text" name="grade10Text">
+                        </div>
+                          <!-- //Secondary & SHS Input Field -->
+                          <div class="form-group" id="grade7shsInput" style="display: none;">
+                            <label for="grade7shsText">Grade 7:</label>
+                            <input type="text" id="grade7shsText" name="grade7shsText">
+                        </div>
+                        <div class="form-group" id="grade8shsInput" style="display: none;">
+                            <label for="grade8shsText">Grade 8:</label>
+                            <input type="text" id="grade8shsText" name="grade8shsText">
+                        </div>
+                        <div class="form-group" id="grade9shsInput" style="display: none;">
+                            <label for="grade9shsText">Grade 9:</label>
+                            <input type="text" id="grade9shsText" name="grade9shsText">
+                        </div>
+                        <div class="form-group" id="grade10shsInput" style="display: none;">
+                            <label for="grade10shsText">Grade 10:</label>
+                            <input type="text" id="grade10shsText" name="grade10shsText">
+                        </div>
+                        <div class="form-group" id="grade11shsInput" style="display: none;">
+                            <label for="grade11shsText">Grade 11:</label>
+                            <input type="text" id="grade11shsText" name="grade11shsText">
+                        </div>
+                        <div class="form-group" id="grade12shsInput" style="display: none;">
+                            <label for="grade12shsText">Grade 12:</label>
+                            <input type="text" id="grade12shsText" name="grade12shsText">
+                         </div>
+                        <!-- //Secondary & SHS Input Field -->
+                        <div class="form-group" id="grade11Input" style="display: none;">
+                            <label for="grade11Text">Grade 11:</label>
+                            <input type="text" id="grade11Text" name="grade11Text">
+                        </div>
+                        <div class="form-group" id="grade12Input" style="display: none;">
+                            <label for="grade12Text">Grade 12:</label>
+                            <input type="text" id="grade12Text" name="grade12Text">
+                         </div>
                                            
                     <div class="form-group">
                         <label>Quarters</label>
@@ -118,16 +169,20 @@
     $(document).ready(function () {
         // Function to show/hide input fields based on radio button selection
         function showHideFields() {
-            var selectedOption = $('input[name="elementaryOption"]:checked').val();
-
             // Hide all grade input fields initially
             $('[id^="gradeInput"]').hide();
 
             // Show grade input fields based on the selected radio button
-            if (selectedOption === 'Elementary') {
+            if ($('#elementary').is(':checked')) {
                 $('#grade1Input, #grade2Input, #grade3Input, #grade4Input, #grade5Input, #grade6Input').show();
+            } else if ($('#secondary').is(':checked')) {
+                $('#grade7Input, #grade8Input, #grade9Input, #grade10Input').show();
+            } else if ($('#secondaryandshs').is(':checked')) {
+                $('#grade7shsInput, #grade8shsInput, #grade9shsInput, #grade10shsInput, #grade11shsInput, #grade12shsInput').show();
+            } else if ($('#shs').is(':checked')) {
+                $('#grade11Input, #grade12Input').show();
             } else {
-                // Add logic for other options if needed
+                // Handle other options if needed
             }
         }
 
@@ -135,7 +190,7 @@
         showHideFields();
 
         // Bind the function to the change event of radio buttons
-        $('input[name="elementaryOption"]').on('change', showHideFields);
+        $('input[name^="Option"]').on('change', showHideFields);
     });
 </script>
 
