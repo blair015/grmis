7611e63bb19ca22f0de09e15e0e17085c2e5530e
blair015 <tblairbrian@gmail.com>
2023-11-23 13:45:59 +0800

    Update save_retention.php

--- oomodal/save_retention.php
+++ oomodal/save_retention.php
@@ -18,17 +18,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     switch ($educationOption) {
         case 'Elementary':
             for ($i = 1; $i <= 6; $i++) {
-                $gradeData["grade{$i}"] = $_POST["grade{$i}Text"];
+                $gradeData[] = $_POST["grade{$i}Text"];
             }
             break;
         case 'Secondary':
             for ($i = 7; $i <= 10; $i++) {
-                $gradeData["grade{$i}"] = $_POST["grade{$i}Text"];
+                $gradeData[] = $_POST["grade{$i}Text"];
             }
             break;
         case 'SHS':
-            $gradeData['grade11'] = $_POST['grade11Text'];
-            $gradeData['grade12'] = $_POST['grade12Text'];
+            $gradeData[] = $_POST['grade11Text'];
+            $gradeData[] = $_POST['grade12Text'];
             break;
         default:
             // Handle other cases if needed
@@ -40,17 +40,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         case 'Elementary':
             // Save data to oo_elementary table
             // Adjust the query and table name as needed
-            $query = "INSERT INTO oo_elementary (school_id, school_year, quarter, " . implode(', ', array_keys($gradeData)) . ") VALUES (:school_id, :school_year, :quarter, :" . implode(', :', array_keys($gradeData)) . ")";
+            $query = "INSERT INTO oo_elementary (school_id, school_year, quarter, grade1, grade2, grade3, grade4, grade5, grade6) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
             break;
         case 'Secondary':
             // Save data to oo_secondary table
             // Adjust the query and table name as needed
-            $query = "INSERT INTO oo_secondary (school_id, school_year, quarter, " . implode(', ', array_keys($gradeData)) . ") VALUES (:school_id, :school_year, :quarter, :" . implode(', :', array_keys($gradeData)) . ")";
+            $query = "INSERT INTO oo_secondary (school_id, school_year, quarter, grade7, grade8, grade9, grade10) VALUES (?, ?, ?, ?, ?, ?, ?)";
             break;
         case 'SHS':
             // Save data to oo_shs table
             // Adjust the query and table name as needed
-            $query = "INSERT INTO oo_shs (school_id, school_year, quarter, " . implode(', ', array_keys($gradeData)) . ") VALUES (:school_id, :school_year, :quarter, :" . implode(', :', array_keys($gradeData)) . ")";
+            $query = "INSERT INTO oo_shs (school_id, school_year, quarter, grade11, grade12) VALUES (?, ?, ?, ?, ?)";
             break;
         default:
             // Handle other cases if needed
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Bind parameters
     $types = str_repeat('s', count($gradeData) + 3);  // Assuming all values are strings
-    $statement->bind_param($types, $schoolId, $schoolYear, $quarter, ...array_values($gradeData));
+    $statement->bind_param($types, $schoolId, $schoolYear, $quarter, ...$gradeData);
 
     // Execute the statement
     $result = $statement->execute();
