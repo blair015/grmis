194460252805d1f5351755803103534fde20a0a6
blair015 <tblairbrian@gmail.com>
2024-01-08 10:07:50 +0800

    Update schoolprofile.php

--- schoolprofile.php
+++ schoolprofile.php
@@ -957,14 +957,13 @@ echo 'Error in preparing the SQL statement.';
                     <!-- Use $teacherData to populate your chart -->
                     <canvas id="enrolleeBarChart2" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
                 </div>
-                
             
             <!-- /.card-body -->
         </div>
-        
         <!-- /.card -->
     </div>
     </div>
+</div>
  
   
 <div class="modal fade" id="updateProfileModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
