8a79cf5b1a85e737b71d42a6b6a909db49851f89
blair015 <tblairbrian@gmail.com>
2024-02-28 08:59:53 +0800

    Update teacherprofile.php

--- teacherprofile.php
+++ teacherprofile.php
@@ -151,11 +151,20 @@
                                     </thead>
                                     <tfoot>
                                         <tr>
-                                            <th>Name</th>
+                                        <th>Name</th>
                                             <th>Position</th>
                                             <th>Office</th>
                                             <th>Age</th>
                                             <th>Start date</th>
+                                            <th>Salary</th>
+                                            <th>Salary</th>
+                                            <th>Salary</th>
+                                            <th>Salary</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                            <th>Salary</th>
+                                            <th>Salary</th>
                                             <th>Salary</th>
                                         </tr>
                                     </tfoot>
